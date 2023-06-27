4472d20daba9a20845853ecc1d8a9ad47e56addd
Jon Carifio <carifio24@gmail.com>
2023-06-27 14:24:39 +0000

    Create app in main.ts

--- radwave/src/main.ts
+++ radwave/src/main.ts
@@ -3,3 +3,21 @@ import Vue, { createApp } from "vue";
 import vuetify from "../plugins/vuetify";
 
 import { WWTComponent, wwtPinia } from "@wwtelescope/engine-pinia";
+import { IconButton } from "@minids/common";
+
+import RadWave from "./RadWave.vue";
+
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+createApp(RadWave, {
+  wwtNamespace: "wwt-minids-radwave",
+})
+
+  .use(wwtPinia)
+  .use(vuetify)
+  
+  .component("WorldWideTelescope", WWTComponent)
+  .component("icon-button", IconButton)
+  .component("font-awesome-icon", FontAwesomeIcon)
+  
+  .mount("#app");
