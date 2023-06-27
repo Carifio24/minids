d6f505353659d3bfa44b65afa4e24851ef0bbdc6
Jon Carifio <carifio24@gmail.com>
2023-06-27 17:33:12 +0000

    Allow importing CSV files directly as strings.

--- radwave/src/RadWave.vue
+++ radwave/src/RadWave.vue
@@ -15,17 +15,42 @@
 
 <script lang="ts">
 import { MiniDSBase } from "@minids/common";
+import { SpreadSheetLayer } from "@wwtelescope/engine";
 import { defineComponent } from "vue";
+import { csv } from "d3-fetch";
+
+import majorCatalogCSV from "./assets/Major_Cloud_Catalog_WAVE_subset_WWT.csv";
 
 type SheetType = "text" | "video" | null;
+
+async function parseCsvTable(path: string) {
+  return csv(path, (d) => {
+    return {
+      name: d.name,
+      l: +(d.l ?? ""),
+      b: +(d.b ?? ""),
+      d50: +(d.d50 ?? ""),
+      ra: +(d.ra ?? ""),
+      dec: +(d.dec ?? "")
+    };
+  });
+}
 
 export default defineComponent({
   extends: MiniDSBase,
 
+  mounted() {
+    this.waitForReady().then(async () => {
+      this.setBackgroundImageByName("Solar System");
+      
+    });
+  },
+
   data() {
     return {
       sheet: null as SheetType,
-      showTextTooltip: false
+      showTextTooltip: false,
+      majorCatalogsLayer: null as SpreadSheetLayer | null
     };
   },
 
@@ -44,6 +69,19 @@ export default defineComponent({
       return {
         '--app-content-height': this.showTextSheet ? '66%' : '100%'
       };
+    }
+  },
+
+  methods: {
+    selectSheet(name: SheetType) {
+      if (this.sheet == name) {
+        this.sheet = null;
+        this.$nextTick(() => {
+          this.blurActiveElement();
+        });
+      } else {
+        this.sheet = name;
+      }
     }
   }
 });
